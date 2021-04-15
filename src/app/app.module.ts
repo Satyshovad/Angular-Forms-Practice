defe53db9ecb6e18e906c61199217bcbbca3b8cf
Darmen96 <67601251+Darmen96@users.noreply.github.com>
2021-04-15 17:52:10 +0600

    Angular Forms Practice. Task1 - Model-driven Forms. Task2 - Template-driven Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Task1ModelDrivenFormsComponent } from './task1-model-driven-forms/task1-model-driven-forms.component';
+import { Task2TemplateDrivenFormsComponent } from './task2-template-driven-forms/task2-template-driven-forms.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Task1ModelDrivenFormsComponent,
+    Task2TemplateDrivenFormsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
